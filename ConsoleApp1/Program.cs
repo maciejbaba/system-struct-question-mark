58975d62540f1c7ee7c761eb39fc977e2e8de347
Maciej Bąba <maciek2baba2@gmail.com>
2024-01-17 23:32:45 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Get all types in the 'System' namespace from mscorlib
+            var systemTypes = Assembly.GetAssembly(typeof(int)) // typeof(int) is just a simple way to get mscorlib
+                                .GetTypes()
+                                .Where(t => t.Namespace != null && t.Namespace.StartsWith("System"))
+                                .OrderBy(t => t.Namespace).ThenBy(t => t.Name);
+
+            foreach (var type in systemTypes)
+            {   Console.WriteLine($"{type}");
+                if (type.ToString().Contains("Console"))
+                {
+                    Console.WriteLine($"{type.Namespace}.{type.Name}");
+                }
+            }
+        }
+    }
+}
